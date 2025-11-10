9360e99b392e3ab7ccf0731de9d2615f14b69234
Marcus Henrik Simonsen <mhsi@itu.dk>
2025-11-10 11:12:12 +0100

    Compare with distance to next waypoint and skip if shorter

--- ship.py
+++ ship.py
@@ -162,6 +162,9 @@ class Ship:
         if self.route is None:
             return
 
+        if len(self.route) >= 2 and distance((self.x, self.y), (self.route[-2])) < distance((self.x, self.y), (self.route[-1])):
+            self.route.pop()
+
         # If route length is 1, we want to dock and therefore move as close to the point as possible
         if len(self.route) > 1 and distance((self.x, self.y), self.route[-1]) <= ROUTE_WAYPOINT_DISTANCE:
             self.route.pop()
